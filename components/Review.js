ea239a794a2f7ebe07a61a04ef5b6039ba5a3190
JeanShin <112054551+qaz434@users.noreply.github.com>
2024-05-07 05:42:39 -0400

    Added styling

--- components/Review.js
+++ components/Review.js
@@ -157,7 +157,7 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     marginRight: 10,
-    borderRadius: 5,
+    borderRadius: 50,
     marginTop: 10,
     marginBottom: 13,
   },
